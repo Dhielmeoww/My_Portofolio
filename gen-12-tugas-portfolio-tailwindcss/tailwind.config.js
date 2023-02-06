49425c2b723c6f4cb82b31d82f5fb7667ee594da
Fadhil_Farhisyi <120102640+Dhielmeoww@users.noreply.github.com>
2023-02-06 19:01:35 +0700

    Portofolio Template Digital Marketing

---
+++ gen-12-tugas-portfolio-tailwindcss/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      rotate: {
+        '270' : '270deg',
+      },
+      backgroundImage: {
+        'montain': "url('https://images4.alphacoders.com/117/thumbbig-1179836.webp')",
+        'city' : "url('https://c4.wallpaperflare.com/wallpaper/855/698/460/science-fiction-cityscape-futuristic-star-wars-wallpaper-preview.jpg')",
+        'rose-dragon': "url('https://c4.wallpaperflare.com/wallpaper/712/253/156/anime-yu-gi-oh-5d-s-dragon-izayoi-aki-wallpaper-preview.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
