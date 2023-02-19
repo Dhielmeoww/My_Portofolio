79bbde52b5e37f94a38ba3c4235f506b4f296109
Dhielmeoww <fadhilfarhisyi@gmail.com>
2023-02-19 22:56:16 +0700

    search

--- gen-12-tugas-react-redux-toolkit/src/pages/DeckBuilder.jsx
+++ gen-12-tugas-react-redux-toolkit/src/pages/DeckBuilder.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../Utility/DeckProvider";
 import { ThemeContext } from "../../Utility/DarkMode";
-import { SearchContext } from "../../Utility/SearchProvider";
 
 // const defaultInput = {
 //   name: "",
@@ -26,7 +25,6 @@ function DeckBuilder() {
   //Get Data
   const { card, setCard, exDeck, setExDeck, library, setLibrary } =
     useContext(UserContext);
-    const {search, setSearch} = useContext(SearchContext)
   const { getAllCard } = useContext(UserContext);
   const { getLibraryCard } = useContext(UserContext);
   const { getExtraDeck } = useContext(UserContext);
@@ -81,51 +79,6 @@ function DeckBuilder() {
       getExtraDeck();
     }
   };
-
-  const libRow = (lib) => (
-    <div key={lib.id} className="w-36 mx-5 mb-9">
-    <img src={lib.image} alt="" className="h-48" />
-    <div className="h-[130px] mt-9">
-      <p>
-        <b>Name</b> : {lib.name}
-      </p>
-      <p>
-        <b>Rarity</b> : {lib.rarity}
-      </p>
-      <p>
-        <b>Type</b> : {lib.type}
-      </p>
-    </div>
-    <div className="flex justify-center mt-2">
-      <div>
-        <button
-          className="bg-slate-100 text-black rounded-xl mt-2 p-2"
-          onClick={() => Desc(`${lib.desc}`)}
-        >
-          {" "}
-          Description{" "}
-        </button>
-        </div>
-       <div>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-2 py-2 px-4 rounded"
-          onClick={() => addCardByList(lib)}
-        >
-          Add
-        </button>
-      </div>
-    </div>
-  </div>
-  )
-
-  const handleCard = (library, search) => {
-    return library.filter((card) => card.name.toLowerCase().includes(search.toLowerCase()))
-        .map(libRow);
-}
-
-const handleSearch = (e) => {
-  setSearch(e.target.value)
-}
 
   // //Handling Form
 
@@ -192,20 +145,42 @@ const handleSearch = (e) => {
           </h1>
           <div className="container mb-10">
             <h2 className="font-bold text-2xl text-center">Library</h2>
-            <input className="bg-white"
-                type="text"
-                placeholder="Cari berdasarkan merk / model"
-                value={search}
-                onChange={handleSearch}
-            />
             <div className="flex flex-wrap flex-row justify-center mt-9 w-full">
-
-
-
-            {handleSearch ? handleCard(library, search) : library.map(libRow)}
-
-
-
+              {library.map((lib) => (
+                <div key={lib.id} className="w-36 mx-5 mb-9">
+                  <img src={lib.image} alt="" className="h-48" />
+                  <div className="h-[130px] mt-9">
+                    <p>
+                      <b>Name</b> : {lib.name}
+                    </p>
+                    <p>
+                      <b>Rarity</b> : {lib.rarity}
+                    </p>
+                    <p>
+                      <b>Type</b> : {lib.type}
+                    </p>
+                  </div>
+                  <div className="flex justify-center mt-2">
+                    <div>
+                      <button
+                        className="bg-slate-100 text-black rounded-xl mt-2 p-2"
+                        onClick={() => Desc(`${lib.desc}`)}
+                      >
+                        {" "}
+                        Description{" "}
+                      </button>
+                      </div>
+                     <div>
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-2 py-2 px-4 rounded"
+                        onClick={() => addCardByList(lib)}
+                      >
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
           <hr className="mt-10" />
