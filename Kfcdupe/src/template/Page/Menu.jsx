0d9a0fa0a08ec8f0a074d6f67c64607b3e0b4eca
Dhielmeoww <fadhilfarhisyi@gmail.com>
2023-04-15 18:47:09 +0700

    Kfc Dupe

--- Kfcdupe/src/template/Page/Menu.jsx
+++ Kfcdupe/src/template/Page/Menu.jsx
@@ -3,21 +3,26 @@ import { useContext } from "react";
 import { UserContext } from "../Utility/MenuProvider";
 
 export default function Menu() {
-  const { menuSpecial, getMenuSpecial, init, setInit } = useContext(UserContext);
+  const {
+    menuSpecial,
+    getMenuSpecial,
+    initSp,
+    setInitSp,
+    initCb,
+    setInitCb,
+    menuCombo,
+    getMenuCombo, 
+    // Next, Prev, 
+    id, setID, 
+    PrevCb, PrevSp, NextCb, NextSp
+  } = useContext(UserContext);
+
   console.log(menuSpecial);
 
-  const Next = () => {
-    const incree = init + 1;
-    setInit(incree);
-  };
-
-  const Prev = () => {
-    const decree = init - 1;
-    setInit(decree);
-  };
 
   useEffect(() => {
     getMenuSpecial();
+    getMenuCombo();
   }, []);
 
   return (
@@ -26,47 +31,130 @@ export default function Menu() {
         <div className="flex justify-center my-10">
           <div>
             <ul className="flex flex-row">
-              <li className="mx-5 text-xl font-semibold">
+              <li className="mx-5 text-xl font-semibold hover:scale-125 hover:cursor-pointer">
                 <h1>Special</h1>
               </li>
-              <li className="mx-5 text-xl font-semibold">
+              <li className="mx-5 text-xl font-semibold hover:scale-125 hover:cursor-pointer">
                 <h1>Combo</h1>
               </li>
-              <li className="mx-5 text-xl font-semibold">
+              <li className="mx-5 text-xl font-semibold hover:scale-125 hover:cursor-pointer">
                 <h1>Praktis</h1>
               </li>
-              <li className="mx-5 text-xl font-semibold">
+              <li className="mx-5 text-xl font-semibold hover:scale-125 hover:cursor-pointer">
                 <h1>Ala Carte</h1>
               </li>
-              <li className="mx-5 text-xl font-semibold">
+              <li className="mx-5 text-xl font-semibold hover:scale-125 hover:cursor-pointer">
                 <h1>Drinks</h1>
               </li>
-              <li className="mx-5 text-xl font-semibold">
+              <li className="mx-5 text-xl font-semibold hover:scale-125 hover:cursor-pointer">
                 <h1>Kids Meal</h1>
               </li>
-              <li className="mx-5 text-xl font-semibold">
+              <li className="mx-5 text-xl font-semibold hover:scale-125 hover:cursor-pointer">
                 <h1>Breakfast</h1>
               </li>
-              <li className="mx-5 text-xl font-semibold">
+              <li className="mx-5 text-xl font-semibold hover:scale-125 hover:cursor-pointer">
                 <h1>Coffe</h1>
               </li>
             </ul>
           </div>
         </div>
-        <div className="flex flex-row flex-wrap">
-            {menuSpecial.map((menus) => (
-                <div key={menus.id}>
-                    <img src={menus.image} alt="" />
+        <div className="flex-col px-7 bg-slate-400">
+          <h1 className="font-bold text-5xl pt-7 ml-5">Special</h1>
+          <div className="flex flex-row justify-center content-center">
+            <button id="prevSp" onClick={PrevSp} disabled={initSp == 0}>
+              <p className="font-bold text-3xl mx-9">❮</p>
+            </button>
+            {menuSpecial.length == 0 ? null : (
+              <div className="flex flex-row justify-center content-center h-[400px]">
+                <div>
+                  <img
+                    src={menuSpecial[initSp].image}
+                    className="hover:scale-125"
+                    alt=""
+                  />
+                  <p className="text-lg font-bold text-center mt-4">{menuSpecial[initSp].title}</p>
                 </div>
+                <div>
+                  <img
+                    src={menuSpecial[initSp + 1].image}
+                    className="hover:scale-125"
+                    alt=""
+                  />
+                  <p className="text-lg font-bold text-center  mt-4">{menuSpecial[initSp+1].title}</p>
+                </div>
+                <div>
+                  <img
+                    src={menuSpecial[initSp + 2].image}
+                    className="hover:scale-125"
+                    alt=""
+                  />
+                  <p className="text-lg font-bold text-center  mt-4">{menuSpecial[initSp+2].title}</p>
+                </div>
+              </div>
+            )}
+
+            <button id="nextSp" onClick={NextSp} disabled={menuSpecial.length - 3 == initSp}>
+              <p className="font-bold text-3xl mx-9">❯</p>
+            </button>
+          </div>
+
+          {/* <div className="flex overflow-x-auto h-[400px] p-9">
+            {menuSpecial.map((menus) => (
+              <div key={menus.id} className="mx-5">
+                <img src={menus.image} className="hover:scale-125" alt="" />
+                <p className="text-center font-bold my-4">{menus.title}</p>
+              </div>
             ))}
+          </div> */}
         </div>
-        <div>
-            ini Coba Coba
-            <button onClick={Prev} disabled={init == 0}>Prev</button>
-            {/* <img src={menuSpecial[init].image} alt="" />
-            <img src={menuSpecial[init+1].image} alt="" />
-            <img src={menuSpecial[init+2].image} alt="" /> */}
-            <button onClick={Next} disabled={menuSpecial.length - 3 == init }>Next</button>
+        <div className="flex-col px-7 bg-slate-600">
+          <h1 className="font-bold text-5xl pt-7 ml-5">Combo</h1>
+          <div className="flex flex-row justify-center content-center">
+            <button id="prevCb" onClick={PrevCb} disabled={initCb == 0}>
+              <p className="font-bold text-3xl mx-9">❮</p>
+            </button>
+            {menuCombo.length == 0 ? null : (
+              <div className="flex flex-row justify-center content-center h-[400px]">
+                <div>
+                  <img
+                    src={menuCombo[initCb].image}
+                    className="hover:scale-125"
+                    alt=""
+                  />
+                  <p className="text-lg font-bold text-center mt-4">{menuCombo[initCb].title}</p>
+                </div>
+                <div>
+                  <img
+                    src={menuCombo[initCb + 1].image}
+                    className="hover:scale-125"
+                    alt=""
+                  />
+                  <p className="text-lg font-bold text-center  mt-4">{menuCombo[initCb+1].title}</p>
+                </div>
+                <div>
+                  <img
+                    src={menuCombo[initCb + 2].image}
+                    className="hover:scale-125"
+                    alt=""
+                  />
+                  <p className="text-lg font-bold text-center  mt-4">{menuCombo[initCb+2].title}</p>
+                </div>
+              </div>
+            )}
+
+            <button id="nextCb" onClick={NextCb} disabled={menuSpecial.length - 3 == initCb}>
+              <p  className="font-bold text-3xl mx-9">❯</p>
+            </button>
+          </div>
+
+          {/* <div className="flex overflow-x-auto h-[400px] p-9">
+            {menuCombo.map((menus) => (
+              <div key={menus.id} className="mx-5">
+                <img src={menus.image} className="hover:scale-125" alt="" />
+                <p className="text-center font-bold my-4">{menus.title}</p>
+              </div>
+            ))}
+          </div> */}
         </div>
       </div>
     </>
--- Kfcdupe/src/template/Utility/MenuProvider.jsx
+++ Kfcdupe/src/template/Utility/MenuProvider.jsx
@@ -5,7 +5,9 @@ export const UserContext = createContext();
 
 export default function MenuProvider(props) {
   const [menuSpecial, setMenuSpecial] = useState([]);
-  const [init, setInit] = useState(0);
+  const [initSp, setInitSp] = useState(0);
+  const [initCb, setInitCb] = useState(0);
+  const [id, setID] = useState()
   const [menuCombo, setMenuCombo] = useState([]);
   const [menuPraktis, setMenuPraktis] = useState([]);
   const [menuCarte, setMenuCarte] = useState([]);
@@ -20,11 +22,70 @@ export default function MenuProvider(props) {
     console.log(res.data);
   };
 
+  const getMenuCombo = async () => {
+    const res = await axios.get("http://localhost:3000/Combo");
+    setMenuCombo(res.data);
+    console.log(res.data);
+  };
+
+  // const Next = () => {
+  //   if( id == "nextSp"){
+  //     console.log(id);
+  //     let incree = initSp + 1;
+  //     setInitSp(incree);
+  //   } else if (id == "nextCb"){
+  //     let incree = initCb + 1;
+  //     setInitCb(incree);
+  //     console.log(id);
+  //   }
+  // };
+
+  const NextSp = () => {
+      let incree = initSp + 1;
+      setInitSp(incree);
+  };
+
+  const NextCb = () => {
+
+      let incree = initCb + 1;
+      setInitCb(incree);
+      console.log(id);
+  };
+
+  // const Prev = () => {
+  //   if(id == "prevCb"){
+  //     let decree = initCb - 1;
+  //     setInitCb(decree);
+  //   } else if(id == "prevSp"){
+  //     let decree = initSp - 1;
+  //     setInitSp(decree);
+  //   }
+  // };
+
+  const PrevCb = () => {
+   
+      let decree = initCb - 1;
+      setInitCb(decree);
+   
+  };
+
+  const PrevSp = () => {
+    
+      let decree = initSp - 1;
+      setInitSp(decree);
+    
+  };
+
   const shareValue = {
     menuSpecial,
     getMenuSpecial,
-    init,
-    setInit,
+    initSp,
+    setInitSp,
+    initCb,
+    setInitCb,
+    menuCombo, getMenuCombo,
+    // Next, Prev, 
+    id, setID, PrevCb, PrevSp, NextCb, NextSp
     
   };
 
